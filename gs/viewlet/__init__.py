ecc33195383814172ac468044917369e2af0d76c
Michael JasonSmith <mpj17@onlinegroups.net>
2014-02-25 14:49:31 -0800

    General PEP-8 cleanup, and switching to absolute-import

--- gs/viewlet/__init__.py
+++ gs/viewlet/__init__.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 *-*
-from contentprovider import SiteContentProvider  # lint:ok
-from viewlet import SiteViewlet  # lint:ok
-from manager import WeightOrderedViewletManager  # lint:ok
+from __future__ import absolute_import
+#lint:disable
+from .contentprovider import SiteContentProvider
+from .viewlet import SiteViewlet
+from .manager import WeightOrderedViewletManager
+#lint:enable
--- gs/viewlet/viewlet.py
+++ gs/viewlet/viewlet.py
@@ -1,8 +1,22 @@
 # -*- coding: utf-8 -*-
-from contentprovider import SiteContentProvider
+##############################################################################
+#
+# Copyright © 2013 OnlineGroups.net and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+from __future__ import absolute_import
+from .contentprovider import SiteContentProvider
 
 
 class SiteViewlet(SiteContentProvider):
     def __init__(self, context, request, view, manager):
-        SiteContentProvider.__init__(self, context, request, view)
+        super(SiteViewlet, self).__init__(context, request, view)
         self.manager = manager
